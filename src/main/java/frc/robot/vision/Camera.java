124427cbf43399bb7706fb30f5cd10ca729508d6
aatle <168398276+aatle@users.noreply.github.com>
2026-01-10 15:18:40 -0800

    Add vision code

---
+++ src/main/java/frc/robot/vision/Camera.java
@@ -0,0 +1,34 @@
+package frc.robot.vision;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Transform3d;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.targeting.PhotonPipelineResult;
+
+import java.util.Optional;
+
+public class Camera {
+    private PhotonCamera camera;
+    private PhotonPoseEstimator poseEstimator;
+
+    public Camera(String cameraName, Transform3d robotToCameraPose) {
+        this.camera = new PhotonCamera(cameraName);
+        this.poseEstimator =
+                new PhotonPoseEstimator(
+                        AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo),
+                        PhotonPoseEstimator.PoseStrategy.AVERAGE_BEST_TARGETS,
+                        robotToCameraPose);
+    }
+
+    public Optional<EstimatedRobotPose> update() {
+        Optional<EstimatedRobotPose> estimatedPose = Optional.empty();
+        for (PhotonPipelineResult result : camera.getAllUnreadResults()) {
+            estimatedPose = poseEstimator.update(result);
+        }
+        return estimatedPose;
+    }
+}
