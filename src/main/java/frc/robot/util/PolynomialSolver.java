fb73c6771769aec67471aa09854c3bdef3084815
aatle <168398276+aatle@users.noreply.github.com>
2026-01-25 11:38:14 -0800

    feat: add skeleton for PolynomialSolver utility class

---
+++ src/main/java/frc/robot/util/PolynomialSolver.java
@@ -0,0 +1,17 @@
+package frc.robot.util;
+
+public final class PolynomialSolver {
+    private PolynomialSolver() {}
+
+    public static double[] depressedQuarticRealRoots(double a, double c, double d, double e) {
+        return new double[] {};
+    }
+
+    public static double[] cubicRealRoots(double a, double b, double c, double d) {
+        return new double[] {};
+    }
+
+    public static double[] quadraticRealRoots(double a, double b, double c) {
+        return new double[] {};
+    }
+}
