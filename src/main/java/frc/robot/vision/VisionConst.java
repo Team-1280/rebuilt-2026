7034a47b8da6cc712f30dc0e16a3eced8042b8d0
Team 1280 EECS1 <sanramonvalleyrobotics@gmail.com>
2026-01-31 13:58:57 -0800

    feat: update camera transforms to prototype mounts

--- src/main/java/frc/robot/vision/VisionConst.java
+++ src/main/java/frc/robot/vision/VisionConst.java
@@ -12,18 +12,19 @@ public final class VisionConst {
     /** The maximum ambiguity (of "tag flipping") allowable to accept a pipeline result */
     public static final double MAX_AMBIGUITY = 0.2;
 
+    // Prototype mounts
     public static final Transform3d FRONT_CAMERA_TRANSFORM =
             new Transform3d(
                     new Translation3d(
-                            Units.inchesToMeters(13.0),
-                            Units.inchesToMeters(5.0),
-                            Units.inchesToMeters(6.0)),
-                    new Rotation3d(0.0, Math.toRadians(20), Math.toRadians(0)));
+                            Units.inchesToMeters(10.876112),
+                            Units.inchesToMeters(9.499361),
+                            Units.inchesToMeters(9.310833)),
+                    new Rotation3d(0.0, Math.toRadians(28.125), Math.toRadians(90)));
     public static final Transform3d BACK_CAMERA_TRANSFORM =
             new Transform3d(
                     new Translation3d(
-                            Units.inchesToMeters(13.25),
-                            Units.inchesToMeters(-2.25),
-                            Units.inchesToMeters(4.75)),
-                    new Rotation3d(0.0, 0.0, Math.toRadians(160)));
+                            Units.inchesToMeters(-10.876112),
+                            Units.inchesToMeters(9.499361),
+                            Units.inchesToMeters(9.310833)),
+                    new Rotation3d(0.0, 28.125, Math.toRadians(-90)));
 }
