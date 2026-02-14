964b73db0aa182c1e54412a435d7c34b5e0449d7
Yingtoes <Wendyytzhao@gmail.com>
2026-02-14 11:40:11 -0800

    fix: feeder added current limits, neutral mode, and inversion direction

--- src/main/java/frc/robot/launcher/feeder/FeederConfig.java
+++ src/main/java/frc/robot/launcher/feeder/FeederConfig.java
@@ -1,13 +1,25 @@
 package frc.robot.launcher.feeder;
 
+import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
 
 public final class FeederConfig {
+    public static final AngularVelocity MOTOR_SPEED = RotationsPerSecond.of(-1); // TODO
+    public static final Current MOTOR_CURRENT_LIMIT = Amps.of(80);
+
     public static final TalonFXConfiguration motorConfig = new TalonFXConfiguration();
 
-    public static final AngularVelocity MOTOR_SPEED = RotationsPerSecond.of(-1); // TODO
+    static {
+        motorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+        motorConfig.CurrentLimits.StatorCurrentLimit = MOTOR_CURRENT_LIMIT.in(Amps);
+        motorConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        motorConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive; // TODO
+    }
 }
---
+++ src/main/java/frc/robot/spindexer/Spindexer.java
@@ -0,0 +1,5 @@
+package frc.robot.spindexer;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Spindexer extends SubsystemBase {}
