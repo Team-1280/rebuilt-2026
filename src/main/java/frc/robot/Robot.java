a167bea690144d0a384907b563ada4248505b423
aatle <168398276+aatle@users.noreply.github.com>
2026-02-13 18:56:04 -0800

    fix: disable simulation replay for now

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,11 +27,9 @@ import frc.robot.drivetrain.CommandSwerveDrivetrain;
 import frc.robot.drivetrain.OdometryDrivetrain;
 import frc.robot.vision.VisionSubsystem;
 
-import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
-import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 public class Robot extends LoggedRobot implements Sendable {
@@ -63,13 +61,14 @@ public class Robot extends LoggedRobot implements Sendable {
             Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
             Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
         } else {
-            setUseTiming(false); // Run as fast as possible
-            // Pull the replay log from AdvantageScope (or prompt the user)
-            String logPath = LogFileUtil.findReplayLog();
-            // Read replay log
-            Logger.setReplaySource(new WPILOGReader(logPath));
-            // Save outputs to a new log
-            Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+            // TODO: add better way to choose if to replay
+            // setUseTiming(false); // Run as fast as possible
+            // // Pull the replay log from AdvantageScope (or prompt the user)
+            // String logPath = LogFileUtil.findReplayLog();
+            // // Read replay log
+            // Logger.setReplaySource(new WPILOGReader(logPath));
+            // // Save outputs to a new log
+            // Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
         }
         // Start logging! No more data receivers, replay sources, or metadata values may
         // be added.
