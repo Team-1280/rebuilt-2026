d2979d540b64f3ac5b6b7bc1b1b480abeabe75c2
aatle <168398276+aatle@users.noreply.github.com>
2026-02-16 15:56:29 -0800

    refactor: minor simplifications

--- src/main/java/frc/robot/intake/IntakeConfig.java
+++ src/main/java/frc/robot/intake/IntakeConfig.java
@@ -8,14 +8,13 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.units.measure.Current;
 
-public class IntakeConfig {
+public final class IntakeConfig {
     public static final double ROLLER_SPEED = 0.0; // TODO
 
     public static final TalonFXConfiguration deployMotorConfig = new TalonFXConfiguration(); // TODO
     public static final Current DEPLOY_MOTOR_CURRENT_LIMIT = Amps.of(80); // TODO
 
     static {
-        deployMotorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
         deployMotorConfig.CurrentLimits.StatorCurrentLimit = DEPLOY_MOTOR_CURRENT_LIMIT.in(Amps);
         deployMotorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
         deployMotorConfig.MotorOutput.Inverted =
@@ -26,7 +25,6 @@ public class IntakeConfig {
     public static final Current ROLLER_MOTOR_CURRENT_LIMIT = Amps.of(80); // TODO
 
     static {
-        rollerMotorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
         rollerMotorConfig.CurrentLimits.StatorCurrentLimit = ROLLER_MOTOR_CURRENT_LIMIT.in(Amps);
         rollerMotorConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
         rollerMotorConfig.MotorOutput.Inverted =
--- src/main/java/frc/robot/intake/IntakeConst.java
+++ src/main/java/frc/robot/intake/IntakeConst.java
@@ -4,7 +4,7 @@ import static edu.wpi.first.units.Units.Degrees;
 
 import edu.wpi.first.units.measure.Angle;
 
-public class IntakeConst {
+public final class IntakeConst {
     public static final int DEPLOY_MOTOR_ID = 0; // TODO
     public static final int ROLLER_MOTOR_ID = -1; // TODO
 
