cb28828115103f7294ab41d89fd957dc38f2aed3
aatle <168398276+aatle@users.noreply.github.com>
2026-02-21 23:02:49 -0800

    feat: replace trajectory optimizer iterations with optimizer tolerance

--- src/main/java/frc/robot/trajectory/TrajectoryConfig.java
+++ src/main/java/frc/robot/trajectory/TrajectoryConfig.java
@@ -10,4 +10,13 @@ public final class TrajectoryConfig {
 
     /** Fraction from 0 to 1 of the maximum speed to use, when a maximal speed is desired */
     public static final double MAXIMAL_SPEED_FRACTION = 0.9; // TODO: tune MAXIMAL_SPEED_FRACTION
+
+    /**
+     * The amount of max possible error in pitch, from the optimal pitch, that the optimizer
+     * algorithm accepts. This determines how precise and how expensive the optimizer is.
+     */
+    public static final double OPTIMIZER_PITCH_TOLERANCE = 0.1; // TODO: tune
+
+    /** An auxiliary upper cap on the number iterations in the pitch optimize algorithm. */
+    public static final int OPTIMIZER_MAX_ITERATIONS = 12;
 }
