e334d1a050c96bb930c93955c53f5ae8a2549ac8
aatle <168398276+aatle@users.noreply.github.com>
2026-02-24 23:27:54 -0800

    feat: add hub dimensions to field constants

--- src/main/java/frc/robot/field/FieldConst.java
+++ src/main/java/frc/robot/field/FieldConst.java
@@ -13,23 +13,31 @@ public final class FieldConst {
     /** Depth of each alliance zone along the x axis */
     public static final Distance ALLIANCE_ZONE_DEPTH = Inches.of(158.6);
 
+    // Hub dimensions
+    public static final Distance BLUE_HUB_X = Inches.of(182.2);
+    public static final Distance RED_HUB_X = FIELD_LENGTH.minus(BLUE_HUB_X);
+    public static final Distance HUB_Y = FIELD_WIDTH.div(2);
+    public static final Distance HUB_SIZE = Inches.of(47.0); // hub base is a square
+    public static final Distance HUB_FUNNEL_INNER_RADIUS = Inches.of(41.7).div(2);
+    public static final Distance HUB_FUNNEL_OUTER_RADIUS =
+            HUB_FUNNEL_INNER_RADIUS.times(2 / Math.sqrt(3)); // by hexagon geometry
+    public static final Distance HUB_HEIGHT = Inches.of(72.0); // to top of funnel
+
     /* Side length of the square robot bumpers */
     public static final Distance ROBOT_SIZE = Inches.of(27);
     public static final Distance ROBOT_HALF_SIZE = ROBOT_SIZE.div(2);
 
-    // bump dimensions (for FieldZoning)
+    // Bump dimensions (for FieldZoning)
     protected static final double BUMP_WIDTH = Inches.of(73.0).in(Meters);
     protected static final double BUMP_DEPTH = Inches.of(44.4).in(Meters);
     protected static final double BUMP_HEIGHT = Inches.of(6.513).in(Meters);
     protected static final double BUMP_HALF_WIDTH = BUMP_WIDTH / 2.0;
     protected static final double BUMP_HALF_DEPTH = BUMP_DEPTH / 2.0;
-    // bumps center offset by half depth
-    protected static final double RED_BUMP_CENTER_X = 11.946; // 12.51 - 0.564
-    protected static final double BLUE_BUMP_CENTER_X = 4.594; // 4.03 + 0.564
-    // From field dimension drawing sheet 4: HUB center at 158.84 inches
-    // Pattern: (73.00) (47.00) (73.00) - BUMP HUB BUMP
-    // Right bump: 158.84 - 23.5 - 36.5 = 98.84 inches = 2.510m
-    // left bump: 158.84 + 23.5 + 36.5 = 218.84 inches = 5.558m
-    protected static final double LEFT_BUMP_CENTER_Y = 5.558; // 218.84 inches
-    protected static final double RIGHT_BUMP_CENTER_Y = 2.510; // 98.84 inches
+    protected static final double BLUE_BUMP_CENTER_X = BLUE_HUB_X.in(Meters);
+    protected static final double RED_BUMP_CENTER_X = RED_HUB_X.in(Meters);
+    protected static final double RIGHT_BUMP_CENTER_Y =
+            HUB_Y.in(Meters) - HUB_SIZE.in(Meters) / 2 - BUMP_HALF_WIDTH;
+    protected static final double LEFT_BUMP_CENTER_Y = FIELD_WIDTH.in(Meters) - RIGHT_BUMP_CENTER_Y;
+
+    // See: https://firstfrc.blob.core.windows.net/frc2026/FieldAssets/2026-field-dimension-dwgs.pdf
 }
