1ce9f2c7a293847b9ea4ff412a6ee092074c76f3
aatle <168398276+aatle@users.noreply.github.com>
2026-02-28 16:23:57 -0800

    feat: add intake angle constants

--- src/main/java/frc/robot/intake/IntakeConst.java
+++ src/main/java/frc/robot/intake/IntakeConst.java
@@ -5,9 +5,9 @@ import static edu.wpi.first.units.Units.Degrees;
 import edu.wpi.first.units.measure.Angle;
 
 public final class IntakeConst {
-    public static final int DEPLOY_MOTOR_ID = 0; // TODO
+    public static final int DEPLOY_MOTOR_ID = -1; // TODO
     public static final int ROLLER_MOTOR_ID = -1; // TODO
 
-    public static final Angle MIN_ANGLE = Degrees.of(0); // TODO
-    public static final Angle MAX_ANGLE = Degrees.of(0); // TODO
+    public static final Angle MIN_ANGLE = Degrees.of(0.00);
+    public static final Angle MAX_ANGLE = Degrees.of(128.261834);
 }
