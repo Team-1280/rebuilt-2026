637ff98eb91b7b7f45094abe2b044e3b70630468
Team 1280 EECS1 <sanramonvalleyrobotics@gmail.com>
2026-03-02 17:31:01 -0800

    feat: add shooter motor ids and direction

--- src/main/java/frc/robot/launcher/shooter/ShooterConst.java
+++ src/main/java/frc/robot/launcher/shooter/ShooterConst.java
@@ -5,9 +5,8 @@ import static edu.wpi.first.units.Units.Inches;
 import edu.wpi.first.units.measure.Distance;
 
 public final class ShooterConst {
-    // TODO: add correct motor ids
-    public static final int RIGHT_LEADER_MOTOR_ID = -1; // right motor from perspective of launcher
-    public static final int LEFT_FOLLOWER_MOTOR_ID = -1; // left motor from perspective of launcher
+    public static final int RIGHT_LEADER_MOTOR_ID = 14; // right motor from perspective of launcher
+    public static final int LEFT_FOLLOWER_MOTOR_ID = 15; // left motor from perspective of launcher
 
     // flywheel and backrollers - 1:1 pulley ratios currently
     public static final Distance FLYWHEEL_RADIUS = Inches.of(2.0);
