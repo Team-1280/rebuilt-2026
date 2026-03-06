135b77e3b4a745c0ffac909a773a9d18af4f239d
Team 1280 EECS1 <sanramonvalleyrobotics@gmail.com>
2026-03-05 16:00:44 -0800

    feat: add tentative turret min and max angles

--- src/main/java/frc/robot/launcher/turret/TurretConst.java
+++ src/main/java/frc/robot/launcher/turret/TurretConst.java
@@ -8,8 +8,9 @@ public final class TurretConst {
     public static final int MOTOR_ID = -1; // TODO
     public static final int ENCODER_ID = -1; // TODO
 
-    public static final Angle MIN_ANGLE = Degrees.of(0); // TODO
-    public static final Angle MAX_ANGLE = Degrees.of(0); // TODO
+    // TODO: these are tentative, conservative estimates
+    public static final Angle MIN_ANGLE = Degrees.of(-315); // TODO
+    public static final Angle MAX_ANGLE = Degrees.of(135); // TODO
 
     /** Number of encoder rotations for every mechanism rotation */
     public static final double ENCODER_TO_MECHANISM_RATIO = 8.5;
