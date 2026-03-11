78075fa730d2e5d0e0aebfe633863b3cd38a475d
Team 1280 EECS1 <sanramonvalleyrobotics@gmail.com>
2026-03-10 17:15:31 -0700

    fix: fix intake gear ratio constant

--- src/main/java/frc/robot/intake/IntakeConst.java
+++ src/main/java/frc/robot/intake/IntakeConst.java
@@ -11,5 +11,5 @@ public final class IntakeConst {
     public static final Angle MIN_ANGLE = Degrees.of(0.00);
     public static final Angle MAX_ANGLE = Degrees.of(128.261834);
 
-    public static final double DEPLOY_ROTOR_TO_MECHANISM_RATIO = 45.0;
+    public static final double DEPLOY_ROTOR_TO_MECHANISM_RATIO = 96.0; // TODO: verify
 }
