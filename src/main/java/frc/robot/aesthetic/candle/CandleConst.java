05d27e69bafe9934c51a5a531c760c15949a6b97
aatle <168398276+aatle@users.noreply.github.com>
2026-03-11 17:33:17 -0700

    tune: add LED count for candle

--- src/main/java/frc/robot/aesthetic/candle/CandleConst.java
+++ src/main/java/frc/robot/aesthetic/candle/CandleConst.java
@@ -2,5 +2,5 @@ package frc.robot.aesthetic.candle;
 
 public final class CandleConst {
     public static final int ID = 27;
-    public static final int LED_COUNT = -1; // TODO: set actual LED count
+    public static final int LED_COUNT = 134;
 }
