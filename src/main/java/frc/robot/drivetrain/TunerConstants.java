15c2edb6a7a74d91596735ef2668b01010e6d467
aatle <168398276+aatle@users.noreply.github.com>
2026-03-13 00:12:52 -0700

    fix: undo erroneous tuner constant changes

--- src/main/java/frc/robot/drivetrain/TunerConstants.java
+++ src/main/java/frc/robot/drivetrain/TunerConstants.java
@@ -40,8 +40,7 @@ import edu.wpi.first.units.measure.Voltage;
 public class TunerConstants {
     // Both sets of gains need to be tuned to your individual robot.
 
-    // The steer motor uses any SwerveModule.SteerRequestType control request with
-    // the
+    // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains =
             new Slot0Configs()
@@ -79,18 +78,9 @@ public class TunerConstants {
     // This needs to be tuned to your individual robot
     private static final Current kSlipCurrent = Amps.of(120);
 
-    // Initial configs for the drive and steer motors and the azimuth encoder; these
-    // cannot be null.
-    // Some configs will be overwritten; check the `with*InitialConfigs()` API
-    // documentation.
-    private static final TalonFXConfiguration driveInitialConfigs =
-            new TalonFXConfiguration()
-                    .withCurrentLimits(
-                            new CurrentLimitsConfigs()
-                                    .withStatorCurrentLimit(Amps.of(120))
-                                    .withStatorCurrentLimitEnable(true)
-                                    .withSupplyCurrentLimit(Amps.of(70))
-                                    .withSupplyCurrentLimitEnable(true));
+    // Initial configs for the drive and steer motors and the azimuth encoder; these cannot be null.
+    // Some configs will be overwritten; check the `with*InitialConfigs()` API documentation.
+    private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration();
     private static final TalonFXConfiguration steerInitialConfigs =
             new TalonFXConfiguration()
                     .withCurrentLimits(
