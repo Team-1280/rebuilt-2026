9e9aa87058a3d835b0d148c4bc635afb7df6c925
Team 1280 EECS1 <sanramonvalleyrobotics@gmail.com>
2026-03-19 19:55:16 -0700

    tune: adjust drive current limits

--- src/main/java/frc/robot/drivetrain/TunerConstants.java
+++ src/main/java/frc/robot/drivetrain/TunerConstants.java
@@ -84,7 +84,14 @@ public class TunerConstants {
     // cannot be null.
     // Some configs will be overwritten; check the `with*InitialConfigs()` API
     // documentation.
-    private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration();
+    private static final TalonFXConfiguration driveInitialConfigs =
+            new TalonFXConfiguration()
+                    .withCurrentLimits(
+                            new CurrentLimitsConfigs()
+                                    .withStatorCurrentLimit(kSlipCurrent)
+                                    .withStatorCurrentLimitEnable(true)
+                                    .withSupplyCurrentLimit(Amps.of(70))
+                                    .withSupplyCurrentLimitEnable(true));
     private static final TalonFXConfiguration steerInitialConfigs =
             new TalonFXConfiguration()
                     .withCurrentLimits(
@@ -94,7 +101,9 @@ public class TunerConstants {
                                     // stator current limit to help avoid brownouts without
                                     // impacting performance.
                                     .withStatorCurrentLimit(Amps.of(50))
-                                    .withStatorCurrentLimitEnable(true));
+                                    .withStatorCurrentLimitEnable(true)
+                                    .withSupplyCurrentLimit(Amps.of(30))
+                                    .withSupplyCurrentLimitEnable(true));
     private static final CANcoderConfiguration encoderInitialConfigs = new CANcoderConfiguration();
     // Configs for the Pigeon 2; leave this null to skip applying Pigeon 2 configs
     private static final Pigeon2Configuration pigeonConfigs =
